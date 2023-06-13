c79c3920f1200aee71d83c90fdba11fadd18917a
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-13 16:40:29 +0000

    create traffic circles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light">
+			<div className="light red"></div>
+			<div className="light yellow"></div>
+			<div className="light green"></div>
 		</div>
 	);
 };
