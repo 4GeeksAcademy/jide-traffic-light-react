112bfc9d896e3277ae1361a93992162e5fefd5b2
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-13 19:03:00 +0000

    useState default condition now working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
-//create your first component
 const Home = () => {
 	const [ selectedColor, setSelectedColor ] = useState("red");
+
 	return (
 		<div className="traffic-light">
-			<div className={"light red"+(selectedColor === "red") ? " glow" : ""}></div>
+			<div className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
 			<br/>
-			<div className={"light yellow"+(selectedColor === "yellow") ? " glow" : ""}></div>
+			<div className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
 			<br/>
-			<div className={"light green"+(selectedColor === "green") ? " glow" : ""}></div>
+			<div className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
 		</div>
 	);
 };
