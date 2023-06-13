97eae6f9351762906baed4f4ae2531e2440d9c75
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-13 19:09:48 +0000

    set onClick fn to change color on click

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,11 +5,20 @@ const Home = () => {
 
 	return (
 		<div className="traffic-light">
-			<div className={"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+			<div 
+				onClick={() => setSelectedColor("red")}
+				className={"light red" + (selectedColor === "red" ? " glow" : "")}>
+			</div>
 			<br/>
-			<div className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+			<div 
+				onClick={() => setSelectedColor("yellow")}
+				className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}>
+			</div>
 			<br/>
-			<div className={"light green" + (selectedColor === "green" ? " glow" : "")}></div>
+			<div 
+				onClick={() => setSelectedColor("green")}
+				className={"light green" + (selectedColor === "green" ? " glow" : "")}>
+			</div>
 		</div>
 	);
 };
